01c208a7a3e91ba44edf2e919ad11cbd70f49315
colaiezzi16 <52834959+colaiezzi16@users.noreply.github.com>
2021-10-21 19:05:48 -0400

    initial commit

---
+++ src/main/java/com/example/examplemod/core/itemgroup/TutorialModItemGroup.java
@@ -0,0 +1,21 @@
+package com.example.examplemod.core.itemgroup;
+
+import com.example.examplemod.core.init.ItemInit;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+public class TutorialModItemGroup extends ItemGroup {
+
+	public static final TutorialModItemGroup TUTORIAL_MOD = new TutorialModItemGroup(ItemGroup.GROUPS.length,
+			"tutorial_mod");
+
+	public TutorialModItemGroup(int index, String label) {
+		super(index, label);
+	}
+
+	@Override
+	public ItemStack createIcon() {
+		return new ItemStack(ItemInit.EXAMPLE_ITEM.get());
+	}
+
+}
