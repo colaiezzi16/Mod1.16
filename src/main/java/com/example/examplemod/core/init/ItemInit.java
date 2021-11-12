299cb036ccaf6d3bd743e7b41a12069393ff9242
colaiezzi16 <52834959+colaiezzi16@users.noreply.github.com>
2021-11-11 19:53:34 -0500

    tools and armor

--- src/main/java/com/example/examplemod/core/init/ItemInit.java
+++ src/main/java/com/example/examplemod/core/init/ItemInit.java
@@ -2,8 +2,11 @@ package com.example.examplemod.core.init;
 
 
 import com.example.examplemod.TutorialMod;
+import com.example.examplemod.common.material.CustomArmorMaterial;
+import com.example.examplemod.common.material.CustomToolMaterial;
 import com.example.examplemod.core.itemgroup.TutorialModItemGroup;
-import net.minecraft.item.Item;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,4 +17,40 @@ public class ItemInit {
 
 	public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
 			() -> new Item(new Item.Properties().group(TutorialModItemGroup.TUTORIAL_MOD)));
+
+	public static final RegistryObject<Item> EXAMPLE_SWORD = ITEMS.register("example_sword",
+			() -> new SwordItem(CustomToolMaterial.EXAMPLE_TOOL, 5, -1f,
+					new Item.Properties().group(TutorialModItemGroup.TUTORIAL_MOD)));
+
+	public static final RegistryObject<Item> EXAMPLE_AXE = ITEMS.register("example_axe",
+			() -> new AxeItem(CustomToolMaterial.EXAMPLE_TOOL, 5, -1f,
+					new Item.Properties().group(TutorialModItemGroup.TUTORIAL_MOD)));
+
+	public static final RegistryObject<Item> EXAMPLE_PICKAXE = ITEMS.register("example_pickaxe",
+			() -> new PickaxeItem(CustomToolMaterial.EXAMPLE_TOOL, 5, -1f,
+					new Item.Properties().group(TutorialModItemGroup.TUTORIAL_MOD)));
+
+	public static final RegistryObject<Item> EXAMPLE_SHOVEL = ITEMS.register("example_shovel",
+			() -> new ShovelItem(CustomToolMaterial.EXAMPLE_TOOL, 5, -1f,
+					new Item.Properties().group(TutorialModItemGroup.TUTORIAL_MOD)));
+
+	public static final RegistryObject<Item> EXAMPLE_HOE = ITEMS.register("example_hoe",
+			() -> new HoeItem(CustomToolMaterial.EXAMPLE_TOOL, 5, -1f,
+					new Item.Properties().group(TutorialModItemGroup.TUTORIAL_MOD)));
+
+	public static final RegistryObject<Item> EXAMPLE_HELMET = ITEMS.register("example_helmet",
+			() -> new ArmorItem(CustomArmorMaterial.EXAMPLE_ARMOR, EquipmentSlotType.HEAD,
+					new Item.Properties().group(TutorialModItemGroup.TUTORIAL_MOD)));
+
+	public static final RegistryObject<Item> EXAMPLE_CHESTPLATE = ITEMS.register("example_chestplate",
+			() -> new ArmorItem(CustomArmorMaterial.EXAMPLE_ARMOR, EquipmentSlotType.CHEST,
+					new Item.Properties().group(TutorialModItemGroup.TUTORIAL_MOD)));
+
+	public static final RegistryObject<Item> EXAMPLE_LEGGINGS = ITEMS.register("example_leggings",
+			() -> new ArmorItem(CustomArmorMaterial.EXAMPLE_ARMOR, EquipmentSlotType.LEGS,
+					new Item.Properties().group(TutorialModItemGroup.TUTORIAL_MOD)));
+
+	public static final RegistryObject<Item> EXAMPLE_BOOTS = ITEMS.register("example_boots",
+			() -> new ArmorItem(CustomArmorMaterial.EXAMPLE_ARMOR, EquipmentSlotType.FEET,
+					new Item.Properties().group(TutorialModItemGroup.TUTORIAL_MOD)));
 }
